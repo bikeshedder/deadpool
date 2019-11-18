11e1ca72b590ef49c8bb5edce4238661f536acfe
Michael P. Jung <michael.jung@terreon.de>
2019-11-18 12:47:39 +0100

    Remove duplicated test

--- postgres/src/lib.rs
+++ postgres/src/lib.rs
@@ -123,21 +123,3 @@ impl<'a> Deref for Transaction<'a> {
         &self.txn
     }
 }
-
-#[tokio::main]
-#[test]
-async fn test_postgres() {
-    use std::env;
-    use crate::{Pool, Manager};
-    let mut cfg = tokio_postgres::Config::new();
-    cfg.host("/var/run/postgresql");
-    cfg.user(env::var("USER").unwrap().as_str());
-    cfg.dbname("deadpool");
-    let mgr = Manager::new(cfg, tokio_postgres::NoTls);
-    let pool = Pool::new(mgr, 16);
-    let mut client = pool.get().await.unwrap();
-    let stmt = client.prepare("SELECT 1 + 2").await.unwrap();
-    let rows = client.query(&stmt, &[]).await.unwrap();
-    let value: i32 = rows[0].get(0);
-    assert_eq!(value, 3);
-}
