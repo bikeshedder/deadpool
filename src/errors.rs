93fcb8348f14d4ecad63d6f3237f136c50836d5f
Michael P. Jung <michael.jung@terreon.de>
2019-12-23 05:15:30 +0100

    impl std::error::Error for PoolError and RecycleError
    This fixes #19

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,7 @@
+use std::fmt;
+
 /// This error is returned by the `Manager::recycle` function
+#[derive(Debug)]
 pub enum RecycleError<E> {
     /// Recycling failed for some other reason
     Message(String),
@@ -11,6 +14,19 @@ impl<E> From<E> for RecycleError<E> {
         Self::Backend(e)
     }
 }
+
+impl<E> fmt::Display for RecycleError<E>
+where E: fmt::Display
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Message(msg) => write!(f, "RecycleError::Message: {}", msg),
+            Self::Backend(e) => write!(f, "RecycleError::Backend {}", e),
+        }
+    }
+}
+
+impl<E> std::error::Error for RecycleError<E> where E: std::error::Error {}
 
 /// When `Pool::get` returns a timeout error this enum can be used
 /// to figure out which step caused the timeout.
@@ -38,3 +54,16 @@ impl<E> From<E> for PoolError<E> {
         Self::Backend(e)
     }
 }
+
+impl<E> fmt::Display for PoolError<E>
+where E: fmt::Display
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Timeout(tt) => write!(f, "PoolError::Timeout: {:?}", tt),
+            Self::Backend(e) => write!(f, "PoolError::Backend: {}", e),
+        }
+    }
+}
+
+impl<E> std::error::Error for PoolError<E> where E: std::error::Error {}
