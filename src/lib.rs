ad1e27506e6d0fc9aa0090eab85a46126b8428c8
Michael P. Jung <michael.jung@terreon.de>
2020-01-11 19:26:09 +0100

    Add type aliases for 0.4 compatibility

--- src/lib.rs
+++ src/lib.rs
@@ -21,3 +21,66 @@ pub struct Status {
     /// number of futures waiting for an object.
     pub available: isize,
 }
+
+#[deprecated(
+    since = "0.5.0",
+    note = "Please use `deadpool::managed::Object` directly"
+)]
+/// This struct was moved to `deadpool::managed::Object`.
+pub type Object<T, E> = managed::Object<T, E>;
+
+#[deprecated(
+    since = "0.5.0",
+    note = "Please use `deadpool::managed::Pool` directly"
+)]
+/// This struct was moved to `deadpool::managed::Pool`.
+pub type Pool<T, E> = managed::Pool<T, E>;
+
+#[deprecated(
+    since = "0.5.0",
+    note = "Please use `deadpool::managed::PoolConfig` directly"
+)]
+/// This struct was moved to`deadpool::managed::PoolConfig`.
+pub type PoolConfig = managed::PoolConfig;
+
+#[deprecated(
+    since = "0.5.0",
+    note = "Please use `deadpool::managed::Timeouts` directly"
+)]
+/// This struct was moved to `deadpool::managed::Timeouts`.
+pub type Timeouts = managed::Timeouts;
+
+#[deprecated(
+    since = "0.5.0",
+    note = "Please use `deadpool::managed::PoolError` directly"
+)]
+/// This enum was moved to `deadpool::managed::PoolError`.
+pub type PoolError<E> = managed::PoolError<E>;
+
+#[deprecated(
+    since = "0.5.0",
+    note = "Please use `deadpool::managed::RecycleError` directly"
+)]
+/// This enum was moved to `deadpool::managed::RecycleError`.
+pub type RecycleError<E> = managed::RecycleError<E>;
+
+#[deprecated(
+    since = "0.5.0",
+    note = "Please use `deadpool::managed::TimeoutType` directly"
+)]
+/// This enum was moved to `deadpool::managed::TimeoutType`.
+pub type TimeoutType = managed::TimeoutType;
+
+#[deprecated(
+    since = "0.5.0",
+    note = "Please use `deadpool::managed::Manager` directly"
+)]
+/// This trait was moved to `deadpool::managed::Manager`.
+pub type Manager<T, E> = dyn managed::Manager<T, E>;
+
+#[deprecated(
+    since = "0.5.0",
+    note = "Please use `deadpool::managed::RecycleResult` directly"
+)]
+/// This type was moved to `deadpool::managed::RecycleResult`.
+pub type RecycleResult<E> = managed::RecycleResult<E>;
