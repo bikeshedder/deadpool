aea1edb6c4904badaa916965d6917a29a57c7395
Michael P. Jung <michael.jung@terreon.de>
2020-06-25 14:42:16 +0200

    Add example using high level commands

--- redis/tests/redis.rs
+++ redis/tests/redis.rs
@@ -1,9 +1,15 @@
+fn create_pool() -> deadpool_redis::Pool {
+    use deadpool_redis::Config;
+    let cfg = Config::from_env("REDIS").unwrap();
+    let pool = cfg.create_pool().unwrap();
+    pool
+}
+
 #[tokio::main]
 #[test]
 async fn test_pipeline() {
-    use deadpool_redis::{pipe, Config};
-    let cfg = Config::from_env("REDIS").unwrap();
-    let pool = cfg.create_pool().unwrap();
+    use deadpool_redis::pipe;
+    let pool = create_pool();
     let mut conn = pool.get().await.unwrap();
     let (value,): (String,) = pipe()
         .cmd("SET")
@@ -16,4 +22,15 @@ async fn test_pipeline() {
         .await
         .unwrap();
     assert_eq!(value, "42".to_string());
+}
+
+#[tokio::main]
+#[test]
+async fn test_high_level_commands() {
+    use redis::AsyncCommands;
+    let pool = create_pool();
+    let mut conn = pool.get().await.unwrap();
+    let _: () = conn.set("deadpool/hlc_test_key", 42).await.unwrap();
+    let value: isize = conn.get("deadpool/hlc_test_key").await.unwrap();
+    assert_eq!(value, 42);
 }
