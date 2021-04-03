5e2a8421ebf7f6522ed3aa30e91b0501ccf8f43b
Michael P. Jung <michael.jung@terreon.de>
2021-04-04 01:08:18 +0200

    Add benchmark for unmanaged pool using Criterion

---
+++ benches/unmanaged.rs
@@ -0,0 +1,21 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use deadpool::unmanaged::Pool;
+
+const ITERATIONS: usize = 1_000_000;
+
+#[tokio::main]
+async fn use_pool() {
+    let pool = Pool::new(16);
+    pool.add(()).await.unwrap();
+    for _ in 0..ITERATIONS {
+        pool.get().await.unwrap();
+    }
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("use_pool", |b| b.iter(|| use_pool()));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
