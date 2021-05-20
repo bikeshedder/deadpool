ec6a6a4b387c6b02b1758689bc360381d947e7e2
Michael P. Jung <michael.jung@terreon.de>
2021-05-21 00:53:27 +0200

    Update test-build script

--- test-build.sh
+++ test-build.sh
@@ -15,17 +15,22 @@ cargo build --no-default-features --features managed,unmanaged
 	cd postgres
 	cargo build
 	cargo build --no-default-features
+	cargo build --no-default-features --features rt_tokio_1
+	cargo build --no-default-features --features rt_async-std_1
 )
 
 (
 	cd redis
 	cargo build
-	cargo build --no-default-features
+	cargo build --no-default-features --features rt_tokio_1
+	cargo build --no-default-features --features rt_async-std_1
 )
 
 (
 	cd lapin
 	cargo build
 	cargo build --no-default-features
+	cargo build --no-default-features --features rt_tokio_1
+	cargo build --no-default-features --features rt_async-std_1
 )
 
