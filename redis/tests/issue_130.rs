56dd74b1f2531ebcaf7acbba5049bd8fb6e0339e
Michael P. Jung <michael.jung@terreon.de>
2021-09-10 14:39:53 +0200

    Clean up test

--- redis/tests/issue_130.rs
+++ redis/tests/issue_130.rs
@@ -1,11 +1,14 @@
-use deadpool_redis::Runtime;
+use deadpool_redis::{PoolConfig, Runtime};
 
 #[tokio::test]
 async fn redis_test() {
     let pool = deadpool_redis::Config {
         url: Some("redis://localhost".to_string()),
         connection: None,
-        ..Default::default()
+        pool: Some(PoolConfig {
+            max_size: 1,
+            timeouts: Default::default(),
+        })
     }
     .create_pool(Runtime::Tokio1)
     .unwrap();
@@ -15,8 +18,7 @@ async fn redis_test() {
     let group = "test_group";
     let consumer = "test_consumer";
     {
-        let mut conn = pool.get().await.expect("conn");
-        // let mut conn = pool.get_async_connection().await.expect("conn");
+        let mut conn = pool.try_get().await.expect("conn");
 
         // del anything under the key in case previous test didn't clean up properly
         redis::Cmd::del(&stream_key[..])
@@ -33,8 +35,7 @@ async fn redis_test() {
         println!("created group");
     }
     {
-        let mut conn = pool.get().await.expect("conn");
-        // let mut conn = pool.get_async_connection().await.expect("conn");
+        let mut conn = pool.try_get().await.expect("conn");
 
         // add message to stream
         redis::Cmd::xadd(&stream_key[..], "*", &[("stuff", "stuffer")][..])
@@ -45,51 +46,53 @@ async fn redis_test() {
         println!("added message");
     }
     {
-        let mut conn = pool.get().await.expect("conn");
-        // let mut conn = pool.get_async_connection().await.expect("conn");
+        let mut conn = pool.try_get().await.expect("conn");
 
         // async inf loop with timeout
-        let _ = tokio::time::timeout(std::time::Duration::from_millis(500), async move {
-            // start from the begenning
-            let mut id = "0";
-            loop {
-                let items = redis::Cmd::xread_options(
-                    &[&stream_key[..]],
-                    &[&id[..]],
-                    &redis::streams::StreamReadOptions::default()
-                        .block(0)
-                        .count(10)
-                        .group(&group[..], &consumer[..]),
-                )
-                .query_async::<_, redis::streams::StreamReadReply>(&mut conn)
-                .await
-                .unwrap();
+        let _ = tokio::spawn(tokio::time::timeout(
+            std::time::Duration::from_millis(500),
+            async move {
+                // start from the begenning
+                let mut id = "0";
+                loop {
+                    let items = redis::Cmd::xread_options(
+                        &[&stream_key[..]],
+                        &[&id[..]],
+                        &redis::streams::StreamReadOptions::default()
+                            .block(0)
+                            .count(10)
+                            .group(&group[..], &consumer[..]),
+                    )
+                    .query_async::<_, redis::streams::StreamReadReply>(&mut conn)
+                    .await
+                    .unwrap();
 
-                let mut ctr = 0;
-                for redis::streams::StreamKey { ids, .. } in items.keys {
-                    for redis::streams::StreamId { id: msg_id, .. } in ids {
-                        redis::Cmd::xack(&stream_key[..], &group[..], &[msg_id])
-                            .query_async::<_, ()>(&mut conn)
-                            .await
-                            .unwrap();
-                        ctr += 1;
+                    let mut ctr = 0;
+                    for redis::streams::StreamKey { ids, .. } in items.keys {
+                        for redis::streams::StreamId { id: msg_id, .. } in ids {
+                            redis::Cmd::xack(&stream_key[..], &group[..], &[msg_id])
+                                .query_async::<_, ()>(&mut conn)
+                                .await
+                                .unwrap();
+                            ctr += 1;
+                        }
+                    }
+                    // if no items are returned, this means we've read everything availiable
+                    // block for new items now
+                    if ctr == 0 {
+                        id = ">";
                     }
                 }
-                // if no items are returned, this means we've read everything availiable
-                // block for new items now
-                if ctr == 0 {
-                    id = ">";
-                }
-            }
-        })
+            },
+        ))
         .await
         .unwrap();
 
         println!("ended loop");
     }
+
     {
-        let mut conn = pool.get().await.expect("conn");
-        // let mut conn = pool.get_async_connection().await.expect("conn");
+        let mut conn = pool.try_get().await.expect("conn");
 
         println!("got connection");
 
