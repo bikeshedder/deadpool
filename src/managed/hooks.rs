bc82c5285290079558bad29a6e1a4cd4046512ae
Michael P. Jung <michael.jung@terreon.de>
2021-09-24 15:19:05 +0200

    Rework HookError
    This is the first part of the hook refactoring as the current
    implementation is missing "continue" support.

--- src/managed/hooks.rs
+++ src/managed/hooks.rs
@@ -6,31 +6,60 @@ use async_trait::async_trait;
 
 use super::{metrics::Metrics, Manager};
 
-/// Possible errors returned by [`Hooks`] which will abort the creation and/or
-/// recycling of objects.
+/// Error structure which which can abort the creation and recycling
+/// of objects.
 #[derive(Debug)]
 pub enum HookError<E> {
+    /// This field is used by the pool to know wether to abort the entire
+    /// operation (typically `Pool::get` or `Pool::get_timeout`) should
+    /// be aborted.
+    Continue(Option<HookErrorCause<E>>),
+    /// This is the optional cause of the error as hooks might just want to
+    /// discard an object (abort=false) and not abort.
+    Abort(HookErrorCause<E>),
+}
+
+/// Possible errors returned by [`Hooks`]
+#[derive(Debug)]
+pub enum HookErrorCause<E> {
     /// Hook failed for some other reason.
     Message(String),
+
+    /// Hook failed for some other reason.
+    StaticMessage(&'static str),
 
     /// Error caused by the backend.
     Backend(E),
 }
 
+impl<E> HookError<E> {
+    /// Get optional cause of this error
+    pub fn cause(&self) -> Option<&HookErrorCause<E>> {
+        match self {
+            Self::Continue(option) => option.as_ref(),
+            Self::Abort(cause) => Some(cause),
+        }
+    }
+}
+
 impl<E: fmt::Display> fmt::Display for HookError<E> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Message(msg) => write!(f, "{}", msg),
-            Self::Backend(e) => write!(f, "{}", e),
+        match self.cause() {
+            Some(HookErrorCause::Message(msg)) => write!(f, "{}", msg),
+            Some(HookErrorCause::StaticMessage(msg)) => write!(f, "{}", msg),
+            Some(HookErrorCause::Backend(e)) => write!(f, "{}", e),
+            None => write!(f, "No cause given"),
         }
     }
 }
 
 impl<E: std::error::Error + 'static> std::error::Error for HookError<E> {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match self {
-            Self::Message(_) => None,
-            Self::Backend(e) => Some(e),
+        match self.cause() {
+            Some(HookErrorCause::Message(_)) => None,
+            Some(HookErrorCause::StaticMessage(_)) => None,
+            Some(HookErrorCause::Backend(e)) => Some(e),
+            None => None,
         }
     }
 }
