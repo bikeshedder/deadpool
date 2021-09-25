89418c22763d77887ef312c7f5d7fcf89fd809a4
Michael P. Jung <michael.jung@terreon.de>
2021-09-26 01:07:34 +0200

    Fix docs

--- src/managed/reexports.rs
+++ src/managed/reexports.rs
@@ -2,7 +2,7 @@
 //! by backend implementations in order to avoid direct dependencies
 //! on the `deadpool` crate itself.
 //!
-//! Crates based on [`deadpool::managed`] should include this line:
+//! Crates based on `deadpool::managed` should include this line:
 //! ```rust
 //! pub use deadpool::managed::reexports::*;
 //! ```
--- src/managed/sync.rs
+++ src/managed/sync.rs
@@ -9,7 +9,7 @@ pub mod reexports {
     //! This module is the variant that should be used by *sync*
     //! backends.
     //!
-    //! Crates based on [`deadpool::managed::sync`] should include this line:
+    //! Crates based on `deadpool::managed::sync` should include this line:
     //! ```rust
     //! pub use deadpool::managed::sync::reexports::*;
     //! ```
