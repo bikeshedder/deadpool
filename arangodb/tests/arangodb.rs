638e85a607afc6b9943c6bc2bdeba549dc2ae456
Michael P. Jung <michael.jung@terreon.de>
2021-11-03 10:47:31 +0100

    Fix arangodb test config

--- arangodb/tests/arangodb.rs
+++ arangodb/tests/arangodb.rs
@@ -3,57 +3,48 @@ use serde_1::Deserialize;
 use deadpool_arangodb::{Pool, Runtime};
 
 
+fn default_dbname() -> String {
+    "deadpool".to_string()
+}
+
+
 #[derive(Debug, Default, Deserialize)]
 #[serde(crate = "serde_1")]
 struct Config {
     #[serde(default)]
     arango: deadpool_arangodb::Config,
+    #[serde(default="default_dbname")]
+    dbname: String,
 }
 
 impl Config {
     pub fn from_env() -> Self {
-        let mut cfg = Config::test_default();
+        let mut cfg = config::Config::new();
         cfg.merge(config::Environment::new().separator("__"))
             .unwrap();
-        cfg.try_into().unwrap()
-    }
-
-    pub fn test_default() -> Self {
-        Self {
-            arango: deadpool_arangodb::Config {
-                url: Some("http://localhost:8529".to_string()),
-                username: Some("root".to_string()),
-                password: Some("deadpool".to_string()),
-                use_jwt: true,
-                pool: None,
-            }
-        }
+        let mut cfg = cfg.try_into::<Self>().unwrap();
+        cfg.arango.url.get_or_insert("http://localhost:8529".to_string());
+        cfg
     }
 }
-
-fn create_pool() -> Pool {
-    let cfg = Config::test_default();
-    cfg.arango.create_pool(Runtime::Tokio1).unwrap()
-}
-
-const DB_NAME: &str = "deadpool";
 
 #[tokio::test]
 async fn create_database() {
-    let pool = create_pool();
+    let cfg = Config::from_env();
+    let pool = cfg.arango.create_pool(Runtime::Tokio1).unwrap();
     let conn = pool.get().await.unwrap();
 
-    let result = conn.create_database(DB_NAME).await;
+    let result = conn.create_database(&cfg.dbname).await;
     if let Err(e) = result {
         assert!(false, "Failed to create database: {:?}", e)
     };
-    let result = conn.db(DB_NAME).await;
+    let result = conn.db(&cfg.dbname).await;
     assert!(result.is_ok());
 
-    let result = conn.drop_database(DB_NAME).await;
+    let result = conn.drop_database(&cfg.dbname).await;
     if let Err(e) = result {
         assert!(false, "Failed to drop database: {:?}", e)
     };
-    let result = conn.db(DB_NAME).await;
+    let result = conn.db(&cfg.dbname).await;
     assert!(result.is_err());
 }
