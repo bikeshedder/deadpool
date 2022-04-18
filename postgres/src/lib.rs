4ae4f9e7050615ed68aafd46116e4c5025cd164f
Dmitry Bochkarev <dimabochkarev@gmail.com>
2022-04-18 21:19:59 +0200

    Postgres GenericClient trait implementation for Client

--- postgres/src/lib.rs
+++ postgres/src/lib.rs
@@ -652,3 +652,54 @@ impl GenericClient for Transaction<'_> {
         tokio_postgres::Transaction::query(&self, query, params).await
     }
 }
+
+#[async_trait]
+impl GenericClient for Client {
+    async fn prepare(&self, query: &str) -> Result<Statement, Error> {
+        PgClient::prepare(&self, query).await
+    }
+
+    async fn execute<T>(
+        &self,
+        query: &T,
+        params: &[&(dyn tokio_postgres::types::ToSql + Sync)],
+    ) -> Result<u64, Error>
+    where
+        T: ?Sized + tokio_postgres::ToStatement + Sync + Send,
+    {
+        PgClient::execute(&self, query, params).await
+    }
+
+    async fn query_one<T>(
+        &self,
+        statement: &T,
+        params: &[&(dyn tokio_postgres::types::ToSql + Sync)],
+    ) -> Result<tokio_postgres::Row, Error>
+    where
+        T: ?Sized + tokio_postgres::ToStatement + Sync + Send,
+    {
+        PgClient::query_one(&self, statement, params).await
+    }
+
+    async fn query_opt<T>(
+        &self,
+        statement: &T,
+        params: &[&(dyn tokio_postgres::types::ToSql + Sync)],
+    ) -> Result<Option<tokio_postgres::Row>, Error>
+    where
+        T: ?Sized + tokio_postgres::ToStatement + Sync + Send,
+    {
+        PgClient::query_opt(&self, statement, params).await
+    }
+
+    async fn query<T>(
+        &self,
+        query: &T,
+        params: &[&(dyn tokio_postgres::types::ToSql + Sync)],
+    ) -> Result<Vec<tokio_postgres::Row>, Error>
+    where
+        T: ?Sized + tokio_postgres::ToStatement + Sync + Send,
+    {
+        PgClient::query(&self, query, params).await
+    }
+}
