531457e85207d2a12910af64ee77e21f664171ff
Michael P. Jung <michael.jung@terreon.de>
2025-02-02 13:11:54 +0000

    Improve pool resize test

--- tests/managed_resize.rs
+++ tests/managed_resize.rs
@@ -22,7 +22,11 @@ impl managed::Manager for Manager {
 }
 
 #[tokio::test]
-async fn test_resize() {
+async fn test_grow_reuse_existing() {
+    // Shrink doesn't discard objects currently borrowed from the pool but
+    // keeps track of them so that repeatedly growing and shrinking will
+    // not cause excessive object creation. This logic used to contain a bug
+    // causing an overflow.
     let mgr = Manager {};
     let pool = Pool::builder(mgr).max_size(2).build().unwrap();
     let obj1 = pool.get().await.unwrap();
@@ -30,11 +34,18 @@ async fn test_resize() {
     assert!(pool.status().size == 2);
     assert!(pool.status().max_size == 2);
     pool.resize(0);
+    // At this point the two objects are still tracked
     assert!(pool.status().size == 2);
     assert!(pool.status().max_size == 0);
     pool.resize(1);
+    // Only one of the objects should be returned to the pool
     assert!(pool.status().size == 2);
-    assert!(pool.status().max_size == 2);
+    assert!(pool.status().max_size == 1);
     drop(obj1);
+    // The first drop brings the size to 1.
+    assert!(pool.status().size == 1);
+    assert!(pool.status().max_size == 1);
     drop(obj2);
+    assert!(pool.status().size == 1);
+    assert!(pool.status().max_size == 1);
 }
